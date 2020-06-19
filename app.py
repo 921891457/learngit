b3e379e8cd8b303c34613eba7db1c7bdd09cd6ed
921891457 <56626889+921891457@users.noreply.github.com>
2020-06-19 17:17:43 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,14 @@
-
-from flask import Flask,request,render_template,Blueprint,make_response
+from flask import Flask,request
+import mongo_client
+import json
 app = Flask(__name__)
-cookie = Blueprint('cookie',__name__)
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if request.method=='GET':
-       return render_template('login.html')
-    else:
-       name = request.form.get('username')
-       passowrd = request.form.get('password')
-       res = make_response(render_template('hello.html',username=name,password=passowrd))
-       res.set_cookie('name',name)
-       return res
-@app.route('/hello',methods=['GET','POST'])
-def hello():
-    if request.method=='GET':
-        return render_template('hello.html',username='111',passowrd='111')
+@app.route('/api/data/<database>/<collection>', methods=['GET', 'POST'])
+def api(database,collection):
+    if request.method=='post':
+       body = request.body()
+       json.load(body)#json转字典
+       db = mongo_client.client[database]
+       account = db[collection]
+       account.insert_one(body)
+if __name__ == '__main__':
+    app.run()
